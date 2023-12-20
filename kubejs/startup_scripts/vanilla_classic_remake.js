e92996ee16b8378efbf2053f70b3a11241769b62
BarMaxx <mbarakhtanov@mail.ru>
2023-12-20 21:07:58 +0300

    1.1.3.101 Beta
    - Изменены квесты из ветки Twilight Forest (изменено/добавлено описание, подзаголовок и прочее)
    - Добавлены новые квесты в ветку Nether
    - Изменены квесты из ветки Nether (добавление описание, подзаголовок и прочее)
    - Добавлены новые квесты в ветку Vanilla Classic Remake
    - Добавлено 2 новых кастомных предмета, а также текстуры для них
    - Изменена текстура кастомного предмета Vanilla Classic Remake
    - Добавлено несколько новых структур для использования их в квестах

--- kubejs/startup_scripts/vanilla_classic_remake.js
+++ kubejs/startup_scripts/vanilla_classic_remake.js
@@ -2,4 +2,8 @@
 StartupEvents.registry('item', event => {
 
 	 event.create('vanilla_classic_remake').displayName('Vanilla Classic Remake')
+	 
+	 event.create('singularity').displayName('Singularity')
+	 
+	 event.create('piece_of_artifact').displayName('Piece of Artifact')
 })
